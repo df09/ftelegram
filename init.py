63dc71555b6a0e32984be206fcbfc9174ff877ec
df09 <090dfdf@gmail.com>
2023-02-26 14:15:39 -0500

    upd

--- init.py
+++ init.py
@@ -14,7 +14,7 @@ def dict2yml(data, f, sort=False):
     return True
 def utime(date): return date - timedelta(hours=5)
 def ftime(date): return date.strftime('%H:%M:%S/%d.%m')
-def get_finish_msg(): return f"finish **{channel}** <{ftime(utime(get_offset()))}"
+def get_start_msg(): return f"**{channel}** <{ftime(utime(get_offset()))}"
 def get_render(channel, date, text, msg_id):
     header = f'{channel}: {utime(message.date).strftime("%H:%M %A (%d %b)")}'
     return f'[{header}](https://t.me/{channel}/{msg_id})\n\n{text}'
@@ -25,12 +25,13 @@ def get_offset():
         return last_offset + timedelta(seconds=1)
     return datetime.now() - timedelta(days=3) + timedelta(hours=5)
 def need_send(text):
-    if not text: text = ''
+    if not text: text = ' '
     for incl in filters[channel]['incls']:
         if not re.search(incl, text, re.IGNORECASE):
             print(f'skip (must incl "{incl}")')
             return False
     for excl in filters[channel]['excls']:
+        print(excl)
         if re.search(excl, text, re.IGNORECASE):
             print(f'skip (need excl "{excl}")')
             return False
@@ -43,19 +44,20 @@ def wait(sec):
     for i in tqdm(range(sec)):
         time.sleep(1)
 
-api_id = 29350618
-api_hash = '1d3d60a614af26ab32058f86f68a1536'
+# init vars
 f_filters = 'filters/work.yml'
 f_offsets = 'filters/work-offsets.yml'
 filters = yml2dict(f_filters)
 offsets = yml2dict(f_offsets)
 
+# start cli
+api_id = 29350618
+api_hash = '1d3d60a614af26ab32058f86f68a1536'
 with TelegramClient(f'isushkov_robot', api_id, api_hash) as client:
     # parse channels
     for channel in filters:
-        finish_msg = get_finish_msg()
+        client.loop.run_until_complete(client.send_message('isushkov_filter', get_start_msg()))
         # parse messages
-        # TODO: remove dublicates
         for message in client.iter_messages(channel, reverse=True,
                 offset_date=get_offset(), limit=999):
             print(f'{channel}: {ftime(utime(message.date))}', end=' ')
@@ -71,4 +73,22 @@ with TelegramClient(f'isushkov_robot', api_id, api_hash) as client:
             # save offset
             offsets[channel] = message.date
             dict2yml(offsets, f_offsets)
-        client.loop.run_until_complete(client.send_message('isushkov_filter', finish_msg))
+        # # TODO: remove dublicates
+        # msgs = []
+        # for m in client.iter_messages('isushkov_filter', reverse=True, limit=999):
+        #     if m.text:
+        #         msgs.append(m)
+        # hashs = {}
+        # for i,m in enumerate(msgs):
+        #     print('-------------')
+        #     print(m.date)
+        #     print(m.text)
+        #     length = len(m.text.splitlines())
+        #     if length > 2:
+        #     #     # remove header
+        #     #     m_hash = hash(m.text)
+        #     #     # if hash already exitst:
+        #     #     #     delete msg from channel
+        #     #     # save hash
+        #     print('')
+        #     exit()

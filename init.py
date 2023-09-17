4352cc20c1b2d5f09d1ea2c469ab600766cde4ff
df09 <090dfdf@gmail.com>
2023-09-17 02:34:58 -0400

    ftelegram: stable

--- init.py
+++ init.py
@@ -1,17 +1,55 @@
-from telethon import TelegramClient, errors
 import asyncio
-# from telethon.tl import functions
-from datetime import datetime, timedelta
-from tqdm import tqdm
+import string
 import yaml
 import time
 import re
 import signal
 import sys
+from tqdm import tqdm
+from telethon import TelegramClient, errors
+from datetime import datetime, timedelta
 def signal_handler(sig, frame):
     print("Ctrl+C: exit.")
     sys.exit(0)
 
+# обработка текста
+def count_characters(word):
+    ru_count = 0
+    en_count = 0
+    for char in word:
+        if 'а' <= char <= 'я' or 'А' <= char <= 'Я': ru_count += 1
+        elif 'a' <= char <= 'z' or 'A' <= char <= 'Z': en_count += 1
+    return ru_count, en_count
+def get_lang(word):
+    ru_char, en_char = count_characters(word)
+    return 'ru' if ru_char >= en_char else 'en'
+def replace_chars(word, mode):
+    ru_en_dict = {'а':'a', 'в':'b', 'с':'c', 'е':'e', 'х':'x', 'р':'p', 'о':'o',
+        'у':'y', 'к':'k', 'н':'h', 'м':'m', 'т':'t', 'А':'A', 'В':'B', 'С':'C',
+        'Е':'E', 'Н':'H', 'О':'O', 'Р':'P', 'Т':'T', 'К':'K', 'М':'M', 'У':'Y',
+        'Х':'X'}
+    for k,v in ru_en_dict.items():
+        if mode == 'ru2en': word = word.replace(k,v)
+        if mode == 'en2ru': word = word.replace(v,k)
+    return word
+def fix_en_ru_chars(trash_text):
+    clear_text = ''
+    tokens = re.findall(r'\w+|\d+|\s+|[^\w\d\s]+|.+', trash_text)
+    for token in tokens:
+        # если токен слово
+        if bool(re.match(r'^\w+$', token)):
+            if get_lang(token) == 'ru':
+                token = token.replace('ё', 'е')
+                token = token.replace('й', 'и')
+                token = token.replace('Ё', 'Е')
+                token = token.replace('Й', 'И')
+                token = replace_chars(token, 'en2ru')
+            if get_lang(token) == 'en':
+                token = replace_chars(token, 'ru2en')
+            clear_text += token
+        else:
+            clear_text += token
+    return clear_text
 # formaters
 def yml2dict(f):
     with open(f, encoding='utf-8') as f:
@@ -31,11 +69,14 @@ def flatte_filter_ruls(ruls):
     return flatten
 
 # decorators
-def flatted(f, max_length=100):
-    return f if len(f) < max_length else f[:max_length-3] + "..."
-def pretty_msg(channel, m):
+def cat(text, max_length=100):
+    text = text.replace('\n', ' ')
+    text = re.sub(r'\s', ' ', text)
+    text = re.sub(r' +', ' ', text)
+    return text if len(text) < max_length else text[:max_length-3] + "..."
+def pretty_msg(channel, m, clean_text):
     header = f'{channel.upper()}: {(m.date - timedelta(hours=5)).strftime("%H:%M %A (%d %b)")}'
-    return f'[{header}](https://t.me/{channel}/{m.id})\n\n{m.text}'
+    return f'[{header}](https://t.me/{channel}/{m.id})\n\n{clean_text}'
 # logic
 def get_offset():
     last_offset = offsets[channel]
@@ -47,27 +88,36 @@ def apply_chanel_filters(log_pfx, text, channel_ruls):
     if not text:
         print(f'{log_pfx} - skip: <empty>')
         return False
-    # incls
-    for incl in flatte_filter_ruls(channel_ruls['incls']):
-        if not re.search(incl, text, re.IGNORECASE):
-            print(f'{log_pfx} - skip: incl "{flatted(incl)}"')
-            return False
-    # excls
-    for excl in flatte_filter_ruls(channel_ruls['excls']):
-        if re.search(excl, text, re.IGNORECASE):
-            print(f'{log_pfx} - skip: excl "{flatted(excl)}"')
-            return False
-    # excls-multiline
-    for excl_multi in flatte_filter_ruls(channel_ruls['excls-multi']):
-        if re.search(excl_multi, text, re.IGNORECASE | re.MULTILINE):
-            print(f'{log_pfx} - skip: excl_multi "{flatted(excl_multi)}"')
-            return False
+    try:
+        # incls
+        for pattern in flatte_filter_ruls(channel_ruls['incls']):
+            if not re.search(pattern, text, re.IGNORECASE):
+                print(f'{log_pfx} - skip: incl "{cat(pattern)}"')
+                return False
+        # excls
+        for pattern in flatte_filter_ruls(channel_ruls['excls']):
+            if re.search(pattern, text, re.IGNORECASE):
+                print(f'{log_pfx} - skip: excl "{cat(pattern)}"')
+                return False
+        # excls-multiline
+        for pattern in flatte_filter_ruls(channel_ruls['excls-multi']):
+            if re.search(pattern, text, re.IGNORECASE | re.MULTILINE):
+                print(f'{log_pfx} - skip: excl_multi "{cat(pattern)}"')
+                return False
+    except re.error as e:
+        print("Некорректное правило фильтрации:", pattern)
+        exit()
     return True
-def send_to_recv(log_pfx, render):
-    client.loop.run_until_complete(client.send_message(
-        receiver, render, link_preview=False))
-    l = flatted(re.sub(r'\(https?://\S+|www\.\S+', '', " ".join(render.splitlines()[2:])))
+def run_tqdm(msg, sec, chart=' ##'):
+    print(msg)
+    for i in tqdm(range(sec), ascii=chart):
+        time.sleep(1)
+
+def send_to_recv(log_pfx, render, pause=4):
+    client.loop.run_until_complete(client.send_message(receiver, render, link_preview=False))
+    l = cat(re.sub(r'\(https?://\S+|www\.\S+', '', " ".join(render.splitlines()[2:])))
     print(f'{log_pfx} - SENDED: {l}')
+    run_tqdm(msg=f'{log_pfx} - SENDED: avoid blocking. wait {pause}s...', sec=pause, chart=' ..')
 def process_message(m):
     # обработка длинных строк
     processed_lines = []
@@ -116,16 +166,16 @@ with TelegramClient(f'isushkov_robot', api_id, api_hash) as client:
         if channel.startswith("x_"):
             continue
         for m in client.iter_messages(channel, reverse=True, offset_date=get_offset(), limit=999):
+            if not m.text: continue # skip enmpty messages
+            clean_text = fix_en_ru_chars(m.text)
             log_pfx = f'parse.{channel.upper()}: {(m.date - timedelta(hours=5)).strftime("%H:%M:%S/%d.%m")}'
-            if apply_chanel_filters(log_pfx, m.text, filters[channel]):
-                render = pretty_msg(channel, m)
+            if apply_chanel_filters(log_pfx, clean_text, filters[channel]):
+                render = pretty_msg(channel, m, clean_text)
                 try:
                     send_to_recv(log_pfx, render)
                 except errors.rpcerrorlist.FloodWaitError as e:
                     sec = int(str(e).split()[3]) + 30
-                    print(f'{log_pfx} - wait {sec}s...')
-                    for i in tqdm(range(sec)):
-                        time.sleep(1)
+                    run_tqdm(f'{log_pfx} - wait {sec}s...', sec)
                     send_to_recv(log_pfx, render)
             # upd offset
             offsets[channel] = m.date
@@ -146,29 +196,31 @@ with TelegramClient(f'isushkov_robot', api_id, api_hash) as client:
         client.loop.run_until_complete(client.delete_messages(receiver, message_ids=m_empty_ids))
         print(f'RECEIVER: empty messages was removed:')
         for m in m_empty:
-            l = flatted(f'{str(m)}: "{m_empty[m]}"').replace('\n','\\n')
+            l = cat(f'{str(m)}: "{m_empty[m]}"').replace('\n','\\n')
             print(f'    {l}')
     print(f'RECEIVER: empty messages - DONE.')
-
     # receiver: remove dublicated
     m_hashs = {}
     c = 0
     for m in client.iter_messages(receiver, limit=999):
         if not m.text: continue # skip admins messages
-        m_hash = hash(''.join(m.text.splitlines()[1:]))
+        # prepare msg
+        clear_text = ''.join(m.text.splitlines()[1:])
+        clear_text = re.sub(r'\((tg|http[s]?)://[^\s]*\)', '()', clear_text)
+        clear_text = re.sub(r'[^a-zA-Zа-яА-Я]', '', clear_text)
+        m_hash = hash(clear_text)
         m_dublicated_ids = [mid for mid,mhash in m_hashs.items() if mhash == m_hash]
         if m_dublicated_ids:
             c += 1
             if c == 1:
                 print(f'RECEIVER: dublicated messages was removed:')
             client.loop.run_until_complete(client.delete_messages(receiver, message_ids=[str(m.id)]))
-            l = flatted(re.sub(r'\(https?://\S+|www\.\S+', '', f'{str(m.id)}: "{m.text}"').replace('\n',' '))
+            l = cat(re.sub(r'\(https?://\S+|www\.\S+', '', f'{str(m.id)}: "{m.text}"').replace('\n',' '))
             print(f'    {l}')
         else:
             m_hashs[m.id] = m_hash
     print(f'RECEIVER: dublicated messages - DONE.')
-
-    # save report
+    # receiver: save report
     msgs = []
     for m in client.iter_messages(receiver, reverse=True, offset_date=get_offset(), limit=999):
         msgs.append(process_message(m.text))

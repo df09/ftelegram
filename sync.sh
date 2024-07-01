2896ed141e888e01b93001615990407e60b5e73e
df09 <090dfdf@gmail.com>
2024-07-01 13:22:08 -0400

    sync: upd

--- sync.sh
+++ sync.sh
@@ -3,4 +3,6 @@
 
 git add . && git commit -m 'sync: upd' && git push
 ssh isushkov@files.shellpea.com 'bash -c "cd ftelegram && git pull"'
+ssh root@files.shellpea.com 'bash -c "echo RESTART_CRON_SERVICE start"'
 ssh root@files.shellpea.com 'bash -c "systemctl restart cron.service"'
+ssh root@files.shellpea.com 'bash -c "echo RESTART_CRON_SERVICE done"'

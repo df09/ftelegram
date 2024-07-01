3b179b26908b10da876a708e653b555862a22ca8
df09 <090dfdf@gmail.com>
2024-07-01 13:20:41 -0400

    sync: upd

--- sync.sh
+++ sync.sh
@@ -3,3 +3,4 @@
 
 git add . && git commit -m 'sync: upd' && git push
 ssh isushkov@files.shellpea.com 'bash -c "cd ftelegram && git pull"'
+ssh root@files.shellpea.com 'bash -c "systemctl restart cron.service"'

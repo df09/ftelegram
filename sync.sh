dc2ba2218c9737e83e3dbd7bf2af32e91caef153
df09 <090dfdf@gmail.com>
2024-07-01 13:31:54 -0400

    sync: upd

--- sync.sh
+++ sync.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# syn filters with server
 
+# git push/pull
 git add . && git commit -m 'sync: upd' && git push
 ssh isushkov@files.shellpea.com 'bash -c "cd ftelegram && git pull"'
-ssh root@files.shellpea.com 'bash -c "echo RESTART_CRON_SERVICE start"'
+# sync offsets
+if [[ "$1" == "offsets" ]]; then
+  scp filters/work-offsets.yml isushkov@files.shellpea.com:/home/isushkov/ftelegram/filters/work-offsets.yml
+fi
+# restart cron
+ssh root@files.shellpea.com 'bash -c "echo RESTART_CRON_SERVICE"'
 ssh root@files.shellpea.com 'bash -c "systemctl restart cron.service"'
-ssh root@files.shellpea.com 'bash -c "echo RESTART_CRON_SERVICE done"'
+ssh root@files.shellpea.com 'bash -c "echo exit-status: $?"'
